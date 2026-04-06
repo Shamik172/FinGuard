aeea66508ec87cf3818aa058a94cb5e6056054ef
Shamik Mandal <shamikmandal170@gmail.com>
2026-04-06 12:00:26 +0530

    added request validation using zod and reusable validation middleware

--- backend/src/modules/record/record.routes.js
+++ backend/src/modules/record/record.routes.js
@@ -3,12 +3,16 @@ import * as controller from "./record.controller.js";
 import authMiddleware from "../../middlewares/auth.middleware.js";
 import authorize from "../../middlewares/role.middleware.js";
 
+import validate from "../../middlewares/validate.middleware.js";
+import { recordSchema } from "./record.validation.js";
+
 const router = express.Router();
 
 router.post(
   "/",
   authMiddleware,
   authorize("ADMIN", "ANALYST"),
+  validate(recordSchema),
   controller.create
 );
 
@@ -18,6 +22,7 @@ router.put(
   "/:id",
   authMiddleware,
   authorize("ADMIN", "ANALYST"),
+  validate(recordSchema),
   controller.update
 );
 
